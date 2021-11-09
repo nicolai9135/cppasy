4ea7bbb8cf588abe87c7f3f002be4e535da212b5
Nicolai Vincent Radke <nicolai.radke@rwth-aachen.de>
2021-11-09 17:56:56 +0100

    add text-field to main frame, restructure frame class

--- include/gui/main_frame.hpp
+++ include/gui/main_frame.hpp
@@ -5,22 +5,30 @@
 
 class main_frame : public wxFrame
 {
-public:
-    main_frame();
 private:
     // file menu actions
-    void OnNew(wxCommandEvent& event);
-    void OnOpen(wxCommandEvent& event);
-    void OnSave(wxCommandEvent& event);
+    // void OnNew(wxCommandEvent& event);
+    // void OnOpen(wxCommandEvent& event);
+    // void OnSave(wxCommandEvent& event);
     void OnExit(wxCommandEvent& event);
 
     // synthesis menu actions
     void OnPreferences(wxCommandEvent& event);
     void OnVariables(wxCommandEvent& event);
+    void OnExecute(wxCommandEvent& event);
 
     // help menu actions
-    void OnAbout(wxCommandEvent& event);
-    void OnSMT2(wxCommandEvent& event);
+    // void OnAbout(wxCommandEvent& event);
+    // void OnSMT2(wxCommandEvent& event);
+
+public:
+    main_frame();
+
+    wxMenu *menu_file;
+    wxMenu *menu_synthesis;
+    // wxMenu *menu_help;
+    wxMenuBar *menu_bar;
+    wxTextCtrl *textctrl;
 };
 
 #endif
--- src/gui/main_frame.cpp
+++ src/gui/main_frame.cpp
@@ -5,67 +5,78 @@ main_frame::main_frame()
     : wxFrame(NULL, wxID_ANY, "cppasy")
 {
     // create file menu
-    wxMenu *menu_file = new wxMenu;
-    menu_file->Append(wxID_NEW);
-    menu_file->AppendSeparator();
-    menu_file->Append(wxID_OPEN);
-    menu_file->AppendSeparator();
-    menu_file->Append(wxID_SAVE);
-    menu_file->AppendSeparator();
+    menu_file = new wxMenu;
+    // menu_file->Append(wxID_NEW);
+    // menu_file->AppendSeparator();
+    // menu_file->Append(wxID_OPEN);
+    // menu_file->AppendSeparator();
+    // menu_file->Append(wxID_SAVE);
+    // menu_file->AppendSeparator();
     menu_file->Append(wxID_EXIT);
 
     // create synthesis menu
-    wxMenu *menu_synthesis = new wxMenu;
+    menu_synthesis = new wxMenu;
     menu_synthesis->Append(wxID_PREFERENCES);
     menu_synthesis->AppendSeparator();
     menu_synthesis->Append(id_variables, "Variable Boundaries...", "Set initial variable boundaries (default: [0,1])");
+    menu_synthesis->AppendSeparator();
+    menu_synthesis->Append(wxID_EXECUTE);
 
     // create help menu
-    wxMenu *menu_help = new wxMenu;
-    menu_help->Append(wxID_ABOUT);
-    menu_help->AppendSeparator();
-    menu_help->Append(id_help_smt2, "'.smt2' Formatting", "External link to the '.smt2' language");
+    // menu_help = new wxMenu;
+    // menu_help->Append(wxID_ABOUT);
+    // menu_help->AppendSeparator();
+    // menu_help->Append(id_help_smt2, "'.smt2' Formatting", "External link to the '.smt2' language");
 
     // create menu bar
-    wxMenuBar *menu_bar = new wxMenuBar;
+    menu_bar = new wxMenuBar;
     menu_bar->Append(menu_file, "&File");
     menu_bar->Append(menu_synthesis, "Synthesis");
-    menu_bar->Append(menu_help, "&Help");
+    // menu_bar->Append(menu_help, "&Help");
 
     SetMenuBar( menu_bar );
     CreateStatusBar();
     SetStatusText("Welcome to cppasy");
 
     // file binds
-    Bind(wxEVT_MENU, &main_frame::OnNew, this, wxID_NEW);
-    Bind(wxEVT_MENU, &main_frame::OnOpen, this, wxID_OPEN);
-    Bind(wxEVT_MENU, &main_frame::OnSave, this, wxID_SAVE);
+    // Bind(wxEVT_MENU, &main_frame::OnNew, this, wxID_NEW);
+    // Bind(wxEVT_MENU, &main_frame::OnOpen, this, wxID_OPEN);
+    // Bind(wxEVT_MENU, &main_frame::OnSave, this, wxID_SAVE);
     Bind(wxEVT_MENU, &main_frame::OnExit, this, wxID_EXIT);
 
     // synthesis binds
     Bind(wxEVT_MENU, &main_frame::OnPreferences, this, wxID_PREFERENCES);
     Bind(wxEVT_MENU, &main_frame::OnVariables, this, id_variables);
+    Bind(wxEVT_MENU, &main_frame::OnExecute, this, wxID_EXECUTE);
 
     // help binds
-    Bind(wxEVT_MENU, &main_frame::OnAbout, this, wxID_ABOUT);
-    Bind(wxEVT_MENU, &main_frame::OnSMT2, this, id_help_smt2);
+    // Bind(wxEVT_MENU, &main_frame::OnAbout, this, wxID_ABOUT);
+    // Bind(wxEVT_MENU, &main_frame::OnSMT2, this, id_help_smt2);
+
+    textctrl = new wxTextCtrl(this, -1, wxT(""), wxPoint(-1, -1), wxSize(400, 400), wxTE_MULTILINE);
+
+    // TODO: make teletype work!!!
+    // textctrl->SetDefaultStyle(wxTextAttr(*wxRED));
+    // textctrl->AppendText("Red text\n");
+    // textctrl->SetDefaultStyle(wxTextAttr(wxFONTFAMILY_TELETYPE));
+    // textctrl->AppendText("doesnt look like teletype\n");
 }
 
 // file
-void main_frame::OnNew(wxCommandEvent& event)
-{
-    wxLogMessage("TODO");
-}
-
-void main_frame::OnOpen(wxCommandEvent& event)
-{
-    wxLogMessage("TODO");
-}
-
-void main_frame::OnSave(wxCommandEvent& event)
-{
-    wxLogMessage("TODO");
-}
+// void main_frame::OnNew(wxCommandEvent& event)
+// {
+//     wxLogMessage("TODO");
+// }
+// 
+// void main_frame::OnOpen(wxCommandEvent& event)
+// {
+//     wxLogMessage("TODO");
+// }
+// 
+// void main_frame::OnSave(wxCommandEvent& event)
+// {
+//     wxLogMessage("TODO");
+// }
 
 void main_frame::OnExit(wxCommandEvent& event)
 {
@@ -83,13 +94,18 @@ void main_frame::OnVariables(wxCommandEvent& event)
     wxLogMessage("TODO");
 }
 
-// help
-void main_frame::OnAbout(wxCommandEvent& event)
+void main_frame::OnExecute(wxCommandEvent& event)
 {
     wxLogMessage("TODO");
 }
 
-void main_frame::OnSMT2(wxCommandEvent& event)
-{
-    wxLogMessage("TODO");
-}
+// help
+// void main_frame::OnAbout(wxCommandEvent& event)
+// {
+//     wxLogMessage("TODO");
+// }
+// 
+// void main_frame::OnSMT2(wxCommandEvent& event)
+// {
+//     wxLogMessage("TODO");
+// }
